3b04723cbb6ac547feada85473178ebef90a5b7d
LutzMage <lutz.mage@gmail.com>
2018-04-17 11:26:49 -0500

    connected java module and sails controller

--- app/api/controllers/documentation/uploadfiles.js
+++ app/api/controllers/documentation/uploadfiles.js
@@ -11,33 +11,45 @@ module.exports = {
 
 
   inputs: {
+
   	name: {
-			required: true,
-			type: 'string',
-		},
-		file: {
-			require: true,
-			type: 'string'
-		}
+		required: true,
+		type: 'string',
+	},
+
+	file: {
+		require: true,
+		type: 'string'
+	}
+
   },
 
 
   exits: {
-  	success:{
-			description: 'Archivo subido correctamente.'
-		},
 
-		invalid: {
-			responseType: 'badRequest',
-			description: 'The provided fullName, password and/or email address are invalid.',
-			extendedDescription: 'If this request was sent from a graphical user interface, the request ' +
-				'parameters should have been validated/coerced _before_ they were sent.'
-		},
+  	success:{
+		description: 'Archivo subido correctamente.'
+	},
+
+	invalid: {
+		responseType: 'badRequest',
+		description: 'The provided fullName, password and/or email address are invalid.',
+		extendedDescription: 'If this request was sent from a graphical user interface, the request ' +
+			'parameters should have been validated/coerced _before_ they were sent.'
+	},
+
   },
 
 
   fn: async function (inputs, exits) {
-  	fs.writeFile('.tmp/public/' + me.nit + '/solo_una_prueba.jpeg', inputs.file.split(',')[1], 'base64', function(err){
+  	var rc = await User.findOne({
+      id: this.req.session.userId,
+    });
+    var dir = 'assets/documents/' + rc.nit;
+    if(!fs.existsSync(dir)){
+    	fs.mkdirSync(dir);
+    }
+  	fs.writeFile(dir + '/' + inputs.name, inputs.file.split(',')[1], 'base64', function(err){
 			if(err) throw err;
 			console.log('done');
 		});
