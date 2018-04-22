88c7af0125a26e25d18d0a4f417c226718a93102
Luis Felipe Diaz Valbuena <luis.felipe.diaz.v@gmail.com>
2018-04-21 19:09:39 -0500

    Modificacion de la forma de subir anexos

--- app/api/controllers/documentation/uploadfiles.js
+++ app/api/controllers/documentation/uploadfiles.js
@@ -4,58 +4,54 @@ const fs = require('fs');
 module.exports = {
 
 
-  friendlyName: 'Uploadfiles',
+	friendlyName: 'Uploadfiles',
 
 
-  description: 'Uploadfiles',
+	description: 'Uploadfiles',
 
 
-  inputs: {
+	inputs: {
+		name: {
+			required: true,
+			type: 'string',
+		},
 
-  	name: {
-		required: true,
-		type: 'string',
+		file: {
+			require: true,
+			type: 'string'
+		}
+
 	},
 
-	file: {
-		require: true,
-		type: 'string'
-	}
 
-  },
+	exits: {
 
+		success: {
+			description: 'Archivo subido correctamente.'
+		},
 
-  exits: {
+		invalid: {
+			responseType: 'badRequest',
+			description: 'The provided fullName, password and/or email address are invalid.',
+			extendedDescription: 'If this request was sent from a graphical user interface, the request ' +
+				'parameters should have been validated/coerced _before_ they were sent.'
+		},
 
-  	success:{
-		description: 'Archivo subido correctamente.'
 	},
 
-	invalid: {
-		responseType: 'badRequest',
-		description: 'The provided fullName, password and/or email address are invalid.',
-		extendedDescription: 'If this request was sent from a graphical user interface, the request ' +
-			'parameters should have been validated/coerced _before_ they were sent.'
-	},
 
-  },
-
-
-  fn: async function (inputs, exits) {
-  	var rc = await User.findOne({
-      id: this.req.session.userId,
-    });
-    var dir = 'assets/documents/' + rc.nit;
-    if(!fs.existsSync(dir)){
-    	fs.mkdirSync(dir);
-    }
-  	fs.writeFile(dir + '/' + inputs.name, inputs.file.split(',')[1], 'base64', function(err){
-			if(err) throw err;
+	fn: async function (inputs, exits) {
+		var rc = await User.findOne({
+			id: this.req.session.userId,
+		});
+		var dir = 'assets/documents/' + rc.nit;
+		if (!fs.existsSync(dir)) {
+			fs.mkdirSync(dir);
+		}
+		fs.writeFile(dir + '/' + inputs.name, inputs.file.split(',')[1], 'base64', function (err) {
+			if (err) throw err;
 			console.log('done');
 		});
-    return exits.success();
-
-  }
-
-
+		return exits.success();
+	}
 };
